973e84d275a69f10e2080a298c87db1dc1f6d696
elshafei0 <elshafeimohamed2005@gmail.com>
2024-08-11 04:59:38 +0300

    init

---
+++ Grill.js
@@ -0,0 +1,59 @@
+const div_res = document.getElementById("responsive");
+const header = document.getElementById("header");
+const body = document.getElementById("body");
+const big = document.querySelector(".big-div");
+const nav = document.querySelector(".nav-img");
+const night = document.querySelector(".svg-night");
+const night_resp = document.querySelector(".svg-night-resp");
+
+let currentIndex = 0;
+
+nav.addEventListener("click", function () {
+  currentIndex = 1 - currentIndex;
+  switch (currentIndex) {
+    case 1:
+      {
+        nav.style.transform = "rotate(90deg)";
+        div_res.style.right = "0";
+        night.style.marginRight = "100px";
+      }
+      break;
+    case 0: {
+      nav.style.transform = "rotate(0deg)";
+      div_res.style.right = "-200px";
+      night.style.marginRight = "5px";
+    }
+  }
+});
+night.addEventListener("click", function () {
+  currentIndex = 1 - currentIndex;
+  switch (currentIndex) {
+    case 1:
+      {
+        body.style.backgroundColor = "blue";
+      }
+      break;
+    case 0: {
+      body.style.backgroundColor = "white";
+    }
+  }
+});
+//
+//
+night_resp.addEventListener("click", function () {
+  currentIndex = 1 - currentIndex;
+  switch (currentIndex) {
+    case 1:
+      {
+        body.style.backgroundColor = "black";
+        body.style.color = "white";
+        document.querySelector(".hr").style.backgroundColor = "white";
+      }
+      break;
+    case 0: {
+      body.style.backgroundColor = "white";
+      body.style.color = "black";
+      document.querySelector(".hr").style.backgroundColor = "black";
+    }
+  }
+});
